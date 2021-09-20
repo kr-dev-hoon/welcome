f7fb471a4b54d9727c396540eff5a50e964be4ad
daehoon <kr.dev.hoon@gmail.com>
2021-09-20 09:09:19 +0900

    nave추가, 사진 배열 수정

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,4 @@
-/*
-	Highlights by HTML5 UP
-	html5up.net | @ajlkn
-	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
-*/
+
 
 (function($) {
 
@@ -168,6 +164,107 @@
 					enter: function() { $bg.addClass('active'); },
 					leave: function() { $bg.removeClass('active'); }
 				});
+
+		});
+
+	// Menu.
+	var $menu = $('#menu'),
+		$menuInner;
+
+	$menu.wrapInner('<div class="inner"></div>');
+	$menuInner = $menu.children('.inner');
+	$menu._locked = false;
+
+	$menu._lock = function() {
+
+		if ($menu._locked)
+			return false;
+
+		$menu._locked = true;
+
+		window.setTimeout(function() {
+			$menu._locked = false;
+		}, 350);
+
+		return true;
+
+	};
+
+	$menu._show = function() {
+
+		if ($menu._lock())
+			$body.addClass('is-menu-visible');
+
+	};
+
+	$menu._hide = function() {
+
+		if ($menu._lock())
+			$body.removeClass('is-menu-visible');
+
+	};
+
+	$menu._toggle = function() {
+
+		if ($menu._lock())
+			$body.toggleClass('is-menu-visible');
+
+	};
+
+	$menuInner
+		.on('click', function(event) {
+			event.stopPropagation();
+		})
+		.on('click', 'a', function(event) {
+
+			var href = $(this).attr('href');
+
+			event.preventDefault();
+			event.stopPropagation();
+
+			// Hide.
+			$menu._hide();
+
+			// Redirect.
+			window.setTimeout(function() {
+				window.location.href = href;
+			}, 250);
+
+		});
+
+	$menu
+		.appendTo($body)
+		.on('click', function(event) {
+
+			event.stopPropagation();
+			event.preventDefault();
+
+			$body.removeClass('is-menu-visible');
+
+		})
+		.append('<a class="close" href="#menu">Close</a>');
+
+	$body
+		.on('click', 'a[href="#menu"]', function(event) {
+
+			event.stopPropagation();
+			event.preventDefault();
+
+			// Toggle.
+			$menu._toggle();
+
+		})
+		.on('click', function(event) {
+
+			// Hide.
+			$menu._hide();
+
+		})
+		.on('keydown', function(event) {
+
+			// Hide on escape.
+			if (event.keyCode == 27)
+				$menu._hide();
 
 		});
 
